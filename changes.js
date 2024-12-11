c9bfbf4dee65d147d4a2862468ede6e026de533c
1alikadri <46671564+1alikadri@users.noreply.github.com>
2024-12-11 13:18:13 +0530

    Add files via upload

---
+++ changes.js
@@ -0,0 +1,93 @@
+
+// Bhopal to Vidisha 
+
+
+
+let fare = 210 ;
+let source = "NALLA-SOPARA";
+let source_hindi = "नालासोपारा";
+let destination = "DADAR";
+let destination_hindi = "दादर ";
+let via = "------";
+let adult = 1;
+let child = 0;
+let train = "AC EMU";
+let train_hindi = "ए.सी ईएमयू";
+let train_hindi2 = "ए.सी ईएमयू";
+let date = "11/11/2024";
+let time = "12:30";
+let number = "9691497996";
+let distance = "45";
+
+
+
+
+// sant hirdaram to Vidisha 
+
+/*
+let fare = 270 ;
+let source = "BHOPAL JN";
+let source_hindi = "भोपाल";
+let destination = "DR AMBEDKAR NGR";
+let destination_hindi = "डॉ अंबेडकर नगर";
+let via = "MKC-DWX";
+let adult = 3;
+let child = 0;
+let train = "Express";
+let train_hindi = "एक्सप्रेस";
+let date = "09/10/2024";
+let time = "04:09";
+let number = "9691497996";
+let distance = "239";
+
+
+*/
+
+
+
+// Vidisha to bhopal
+
+/*
+
+let fare = 50 ;
+let source = "Vidisha";
+let source_hindi = "विदिशा";
+let destination = "Bhopal JN.";
+let destination_hindi = "भोपाल";
+let via = "SCI";
+let adult = 1;
+let child = 0;
+let train = "superfast (s)";
+let train_hindi = "सुपरफास्ट";
+let date = "18/07/2024";
+let time = "07:24";
+let number = "9691497996";
+let distance = "53";
+
+
+
+*/
+
+
+
+
+
+// show.html
+document.querySelector("#fare-1").innerHTML = fare;
+document.querySelector("#source-1").innerHTML = source;
+document.querySelector("#destination-1").innerHTML = destination;
+document.querySelector("#via-1").innerHTML = via;
+document.querySelector("#adult-1").innerHTML = adult;
+document.querySelector("#child-1").innerHTML = child;
+document.querySelector("#date-time-1").innerHTML = date+" "+time;
+document.querySelector("#train-1").innerHTML = train;
+
+
+// ticket.html 
+document.querySelector("#date-1").innerHTML = date;
+document.querySelector("#contact-no").innerHTML = number;
+document.querySelector("#distance").innerHTML = distance;
+document.querySelector("#source-hindi").innerHTML = source_hindi;
+document.querySelector("#destination-hindi").innerHTML = destination_hindi;
+document.querySelector("#train-hindi").innerHTML = train_hindi;
+document.querySelector("#destination-2").innerHTML = destination;
